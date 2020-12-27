c69555117bbc475e2912244cb68dee082c53e6ce
Matthew Ma <matthew.ma@wallawalla.edu>
2020-12-26 22:28:36 -0800

    Duplicate logic of checkout page to return page

--- public/return.js
+++ public/return.js
@@ -6,6 +6,7 @@ $(document).ready(() => {
 		if (e.which == 13 && !e.shiftKey) {
 			e.preventDefault();
 			selectDevice()
+			$("#item-id").val("")
 		}
 	})
 
@@ -13,6 +14,7 @@ $(document).ready(() => {
 		if (e.which == 13 && !e.shiftKey) {
 			e.preventDefault();
 			selectDevice()
+			$("#item-id").focus()
 		}
 	})
 

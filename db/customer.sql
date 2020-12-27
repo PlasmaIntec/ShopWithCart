75fef8d580bd90b7e5c748a357f0a7d3aacff0e7
Matthew Ma <matthew.ma@wallawalla.edu>
2020-12-26 19:43:33 -0800

    Add db dump

---
+++ db/customer.sql
@@ -0,0 +1,4 @@
+customerid,customername
+8,Matt
+9,Neff
+10,Olly
---
+++ db/item.sql
@@ -0,0 +1,8 @@
+itemid,itemname,customerid,cartid
+27,TempleOS,,
+24,Windows 10,,
+26,MacOS,,28
+25,Linux,,30
+30,Cart 3,,
+29,Cart 2,,
+28,Cart 1,,
